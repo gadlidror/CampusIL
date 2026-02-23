dccbc243e4e65952ab6bb0e2a70a8370dd6693d7
Gad Lidror <gady.lidror@gmail.com>
2026-02-23 17:07:14 +0200

    Try to put the functions to visiable

--- src/CampusIL/CampusIL.py
+++
@@ -1,6 +0,0 @@
-def _expect_assert(fn, *args):
-  try:
-    fn(*args)
-  except AssertionError:
-    return True
-  return False
--- src/CampusIL/__init__.py
+++ src/CampusIL/__init__.py
@@ -3,3 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import random
 
+def _expect_assert(fn, *args):
+  try:
+    fn(*args)
+  except AssertionError:
+    return True
+  return False
