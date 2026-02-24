248a935a61af4d480f2a75794babad06d604c9bd
Gad Lidror <gady.lidror@gmail.com>
2026-02-24 17:25:13 +0200

    All lowercase and add classes

--- src/CampusIL/__init__.py
+++ src/CampusIL/__init__.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import random
 
 from .utils import expect_assert
-#from .func_families import *
+from .func_families import Line, Square, Poli
 
 
-__all__ = ["expect_assert"]
-#__all__ = ["expect_assert", "Line", "Square", "Poli"]
+#__all__ = ["expect_assert"]
+__all__ = ["expect_assert", "Line", "Square", "Poli"]
